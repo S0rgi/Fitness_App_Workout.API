fa4f7127d7e7edda96c288cb9f82986bfe30b520
S0rgi <ilyuxa.chernov.04@mail.ru>
2026-03-14 23:09:02 +0300

    add workout profile endpoint

--- Fitness_App_Workout.API/Interfaces/IWorkoutService.cs
+++ Fitness_App_Workout.API/Interfaces/IWorkoutService.cs
@@ -1,7 +1,6 @@
 using Fitness_App_Workout.API.Dto;
 using Fitness_App_Workout.API.Grpc;
 using Fitness_App_Workout.API.Models;
-using Microsoft.AspNetCore.Mvc;
 
 namespace Fitness_App_Workout.API.Interfaces;
 
@@ -11,10 +10,11 @@ public interface IWorkoutService
     public Task<GetWorkoutListResult> GetWorkoutList(UserResponse user,WorkoutFilter filter);
     public Task<GetWorkoutResult> GetWorkout(string workoutId,UserResponse user);
     public Task<DeleteWorkoutResult> DeleteWorkout(string workoutId,UserResponse user);
-
+    public Task<WorkoutProfileResult> GetProfile(UserResponse user);
 
 }
 public record CreateWorkoutResult(bool result, string ErrorMessage,Guid WorkoutId);
 public record GetWorkoutListResult(bool result, string ErrorMessage,List<Workout> workouts);
 public record GetWorkoutResult(bool result, string ErrorMessage, WorkoutDto workout);
+public record WorkoutProfileResult(bool result, string ErrorMessage, WorkoutProfileDto workoutProfile);
 public record DeleteWorkoutResult(bool result, string ErrorMessage);
